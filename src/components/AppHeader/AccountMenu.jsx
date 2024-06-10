8b088c21dbdc581ef2f9cb8f7c4bd9b2fe01b829
Sarem Hailemeskel <166467953+pir8bay@users.noreply.github.com>
2024-06-10 00:02:51 +0000

    fix bug

--- src/components/AppHeader/AccountMenu.jsx
+++ src/components/AppHeader/AccountMenu.jsx
@@ -24,9 +24,11 @@ const AccountMenu = ({ profile, open, anchorEl, onClose, ...rest }) => {
 
   const shortenGitHash = fullHash => fullHash ? fullHash.substring(0, 7) : '';
 
-  const gitCommitHash = process.env.REACT_APP_GIT_COMMIT_HASH_PLACEHOLDER || '';
+  // Read the commit hash from the environment variable set during the build process
+  const gitCommitHash = process.env.REACT_APP_GIT_COMMIT_HASH || '';
   const shortGitCommitHash = shortenGitHash(gitCommitHash);
-  const gitCommitUrl = gitCommitHash ? `https://github.com/commaai/openpilot/commit/${shortGitCommitHash}` : '';
+  const gitCommitUrl = gitCommitHash ? `https://github.com/commaai/openpilot/commit/${gitCommitHash}` : '';
+  console.log(gitCommitHash)
 
   return (
     <Menu
